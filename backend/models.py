ad43c37ff2d231066b1c83f0cc2514c6001b0712
Kachukwu Okoh <okohkachukwu@gmail.com>
2022-07-11 07:10:33 +0100

    Updating untracked files

--- backend/models.py
+++ backend/models.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 
 database_name = 'trivia'
-database_path = 'postgresql://{}/{}'.format('localhost:5432', database_name)
+database_path = 'postgresql://{}:{}@{}/{}'.format('postgres','kosime1996','localhost:5432', database_name)
 
 db = SQLAlchemy()
 
@@ -12,6 +12,8 @@ db = SQLAlchemy()
 setup_db(app)
     binds a flask application and a SQLAlchemy service
 """
+
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
