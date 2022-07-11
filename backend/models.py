85a9acdd81bcc02af07b950ba91038f9ef24758e
Kachukwu Okoh <okohkachukwu@gmail.com>
2022-07-11 08:13:25 +0100

    Updating untracked files

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
-# import os
-from sqlalchemy import Column, String, Integer
+import os
+from sqlalchemy import Column, String, Integer, create_engine
 from flask_sqlalchemy import SQLAlchemy
-# import json
+import json
 
 database_name = 'trivia'
 database_path = 'postgresql://{}:{}@{}/{}'.format('postgres','kosime1996','localhost:5432', database_name)
